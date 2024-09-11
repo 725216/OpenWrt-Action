0ed79f84eefc6724b8f64af0bde06e612e84515f
725216 <djp.1@qq.com>
2024-09-11 16:33:59 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -18,4 +18,4 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
-git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone https://github.com/lxiaya/openwrt-homeproxy.git package/openwrt-homeproxy
