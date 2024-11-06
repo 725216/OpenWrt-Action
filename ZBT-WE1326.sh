6f52dca660aaa6249c2e8eeaa45fbfe2895c1368
725216 <djp.1@qq.com>
2024-11-06 08:25:59 +0800

    Update ZBT-WE1326.sh

--- ZBT-WE1326.sh
+++ ZBT-WE1326.sh
@@ -11,3 +11,8 @@
 # 3-添加 PassWall 插件
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/packages/net/xray-plugin
