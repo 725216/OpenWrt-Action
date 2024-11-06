30da3bc758da94c866c732e3ef742955bc8f3089
725216 <djp.1@qq.com>
2024-11-06 16:36:39 +0800

    Update ZBT-WE1326.sh

--- ZBT-WE1326.sh
+++ ZBT-WE1326.sh
@@ -3,10 +3,10 @@
 
 
 # 1-添加 ShadowSocksR Plus+ 插件
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 2-添加 OpenClash 插件
-sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+# sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 3-添加 PassWall 插件
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
@@ -19,3 +19,5 @@ rm -rf feeds/packages/net/xray-plugin
 git clone https://github.com/sbwml/luci-app-alist.git package/alist
 git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
 git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
+git clone https://github.com/fw876/helloworld.git package/hellowrld
+git clone https://github.com/vernesong/OpenClash.git package/openclash
